199bcade1533ee283443847552fa5f7356ebe4a6
Lucas Norato <lucasn.norato@gmail.com>
2024-10-02 13:52:46 -0300

    if, e idiomas

--- hello.py
+++ hello.py
@@ -1,5 +1,34 @@
-print("Hello world!")
-#Este programa imprime hello world
-print("Isso é um print")
-print("V3")
-print("não seja por isso")
+#!/usr/bin/env python3
+"""
+    Hello world multi linguas    
+
+Dependendo da lingua configurada no ambiente o programa exibe a correspondente
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_br
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Lucas Norato"
+__license__ = "Unlicense"
+
+import os.getenv("LANG")[:5]
+
+current_language = "en_Us"
+
+msg = "Hello, World!"
+
+if current_language == "pt-BR" :
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Cyao, Mondo!"
+
+print(msg)
